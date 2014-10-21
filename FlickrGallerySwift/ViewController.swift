be0637eadb2f9e243035250f7ff7553b55c6dcbd
“KEEVIN <keevin.mitchell@gmail.com>
2014-10-21 01:45:50 -0400

    Initial Commit

---
+++ FlickrGallerySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrGallerySwift
+//
+//  Created by KEEVIN MITCHELL on 10/21/14.
+//  Copyright (c) 2014 Beyond 2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
